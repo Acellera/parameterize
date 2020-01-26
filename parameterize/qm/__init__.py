848d7128780ce57e62b652c91ceee3505660353b
Stefan Doerr <stefdoerr@gmail.com>
2020-01-26 11:03:16 +0000

    first commit for move from htme. fixes imports

---
+++ parameterize/qm/__init__.py
@@ -0,0 +1,10 @@
+# (c) 2015-2018 Acellera Ltd http://www.acellera.com
+# All Rights Reserved
+# Distributed under HTMD Software License Agreement
+# No redistribution in whole or part
+#
+from parameterize.qm.psi4 import Psi4
+from parameterize.qm.gaussian import Gaussian
+from parameterize.qm.terachem import TeraChem
+from parameterize.qm.fake import FakeQM, FakeQM2
+from parameterize.qm.base import QMResult
